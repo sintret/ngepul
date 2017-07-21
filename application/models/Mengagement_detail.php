9e2a697c1752eed456463b0d6f09661e08dc61b5
andif_injani <andif@injani.com>
2017-07-21 10:22:09 +0700

    postpay url

--- application/models/Mengagement_detail.php
+++ application/models/Mengagement_detail.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Mengagement extends CI_Model {
+class Mengagement_detail extends CI_Model {
 
     public $table = 'engagementdetail';
     public $id = 'id';
