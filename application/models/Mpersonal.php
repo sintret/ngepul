450d92ecc25cf77b0bb1ebe8edb5d1110e069816
patars <patars@injani.com>
2017-07-23 14:11:21 +0700

    image-logo

---
+++ application/models/Mpersonal.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Mpersonal extends CI_Model
+{
+
+    public $table = 'personal';
+    public $id = 'id';
+    public $order = 'DESC';
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    // get all
+    function get_all()
+    {
+        $this->db->order_by($this->id, $this->order);
+        return $this->db->get($this->table)->result();
+    }
+
+}
--- application/views/personal/change_password.php
+++ application/views/personal/change_password.php
@@ -1,2 +1,31 @@
 
-change password here
+<section class="panel" style="background-color: whitesmoke">
+    <header class="panel-heading btn-inverse">
+        <h4><strong>CHANGE YOUR PASSWORD</strong> </h4>
+    </header>
+    <div class="panel-body">
+        <?= $this->session->flashdata('eroor_set');?>
+        <form action="<?= base_url();?>personal/change_password_add" method="post"  enctype="multipart/form-data">
+
+	    <div class="form-group">
+            <label for="varchar">OLD PASSWORD</label>
+            <input type="password" class="form-control" name="oldPassword" id="oldPassword" placeholder="old password" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">NEW PASSWORD</label>
+            <input type="password" class="form-control" name="newPassword" id="password" placeholder="new password"  />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">RETYPE NEW PASSWORD</label>
+            <input type="password" class="form-control" name="retypeNewPassword" id="retypePassword" placeholder="new password"  />
+        </div>
+            <hr/>
+            <center>    
+                <input type="hidden" name="id" value="<?= $this->session->userdata('id');?>" /> 
+                <a href="<?php echo site_url('personal/change_password') ?>" class="btn btn-default">Cancel</a>
+                <button type="submit" class="btn btn-primary">CHANGE PASSWORD</button> 
+            </center>
+        </form>
+
+</section>
+
