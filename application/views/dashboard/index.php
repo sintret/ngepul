cad04343c7f1d880dc9ef03faf8d5e26b31dd02d
andy fitria <sintret@gmail.com>
2017-07-23 15:42:55 +0700

    compare

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -127,18 +127,23 @@
                             <?php
                             $no = 1;
                             $timesheets = $results['results'];
+                            $ym = $results['ym'];
+                            $ids = $results['ids'];
                             if ($timesheets)
                                 foreach ($timesheets as $timesheet) {
                                     ?>
                                     <tr>
-                                        <td>#</td>
+                                        <td><?php echo $no; ?></td>
                                         <td><?php echo $timesheet->name; ?></td>
                                         <td><?php echo $timesheet->description; ?></td>
                                         <td>Approval</td>
-                                        <?php for ($i = $results['time1']; $i <= $results['time2']; $i++) { ?>
-                                            <td>0</td>
+                                        <?php
+                                        for ($i = $results['time1']; $i <= $results['time2']; $i++) {
+                                            $d = $ym . $i;
+                                            ?>
+                                            <td><button id="ts<?php echo $timesheet->id . '_' . $d; ?>" type="button" data-engagementId="<?= $timesheet->id; ?>" data-title="<?= str_replace('"', "", $timesheet->name . ' with' . $d); ?>" data-no="<?= $no; ?>" data-description="<?= isset($ids[$timesheet->id][$d]['description']) ? str_replace('"', '', $ids[$timesheet->id][$d]['description']) : ''; ?>" data-date="<?= $d; ?>" class="btn btn-link tInput"><?= isset($ids[$timesheet->id][$d]['hour']) ? $ids[$timesheet->id][$d]['hour'] : 0 ?></button></td>
                                         <?php } ?>
-                                        <td>Total</td>
+                                        <td id="total<?php echo $timesheet->id; ?>"></td>
                                     </tr>
                                     <?php
                                     $no++;
@@ -282,19 +287,19 @@
         $("#tDate").val($(this).data("date"));
     });
 
-    $(document).on("click","#tSave", function () {
+    $(document).on("click", "#tSave", function () {
         $.ajax({
             type: "POST",
             url: '<?= base_url(); ?>dashboard/ajax_timesheet_post',
             data: {engagementId: $("#tEngagementId").val(), date: $("#tDate").val(), hour: $("#tHour").val(), description: $("#tDescription").val()},
             success: function (data) {
                 var json = JSON.parse(data);
-                var engagementId =$("#tEngagementId").val();
+                var engagementId = $("#tEngagementId").val();
                 var date = $("#tDate").val();
-                var elm = "ts"+engagementId +"_"+ date;
-                $("#"+elm).html(json.value);
-                $("#total"+engagementId).html(json.total);
-                $('#tModal').modal('hide');                
+                var elm = "ts" + engagementId + "_" + date;
+                $("#" + elm).html(json.value);
+                $("#total" + engagementId).html(json.total);
+                $('#tModal').modal('hide');
             }
         });
     });
