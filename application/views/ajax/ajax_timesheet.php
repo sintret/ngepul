a81287c5ff098d3a13d52d82b8b0555e75030e4f
andy fitria <sintret@gmail.com>
2017-07-30 19:03:09 +0700

    compare

--- application/views/ajax/ajax_timesheet.php
+++ application/views/ajax/ajax_timesheet.php
@@ -13,7 +13,12 @@ if ($timesheets)
             <td>Approval</td>
             <?php
             for ($i = $results['time1']; $i <= $results['time2']; $i++) {
-                $d = $ym . $i;
+                if ($i < 10) {
+                    $dt = '0' . $i;
+                } else {
+                    $dt = $i;
+                }
+                $d = $ym . $dt;
                 ?>
                 <td><button id="ts<?php echo $timesheet->id . '_' . $d; ?>" type="button" data-engagementId="<?= $timesheet->id; ?>" data-title="<?= str_replace('"', "", $timesheet->name . ' with' . $d); ?>" data-no="<?= $no; ?>" data-description="<?= isset($ids[$timesheet->id][$d]['description']) ? str_replace('"', '', $ids[$timesheet->id][$d]['description']) : ''; ?>" data-date="<?= $d; ?>" class="btn btn-link tInput"><?= isset($ids[$timesheet->id][$d]['hour']) ? $ids[$timesheet->id][$d]['hour'] : 0 ?></button></td>
             <?php } ?>
